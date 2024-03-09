6f9599a19112c235fcdea3bc113cb6e22884a156
junyaQ <junyaqiao@gmail.com>
2024-03-08 22:14:37 -0500

    test the deploy

--- api/movies.js
+++ api/movies.js
@@ -8,29 +8,14 @@ const getMovies = () => {
   const data = JSON.parse(dbData);
   return data.movies;
 };
-const getMovieById = (id) => {
-    const dbData = fs.readFileSync(dbPath);
-    const movie = data.movies.find(movie => movie.id === id);
-    return movie;
-  };
 
 module.exports = (req, res) => {
-    switch (req.method) {
-        case 'GET':
-          const { id } = req.params;
-          if (id) {
-            const movie = getMovieById(id);
-            if (movie) {
-              res.json(movie);
-            } else {
-              res.status(404).send('Movie not found');
-            }
-          } else {
-            res.json(getMovies());
-          }
-          break;
-        default:
-          res.status(405).end(); // Method Not Allowed
-          break;
-      }
-    };
+  switch (req.method) {
+    case 'GET':
+      res.json(getMovies());
+      break;
+    default:
+      res.status(405).end(); // Method Not Allowed
+      break;
+  }
+};
