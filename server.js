b440ed8aa87cd30791d32ad7dd51ce603d37ff09
junyaQ <junyaqiao@gmail.com>
2024-03-08 21:36:24 -0500

    add for id routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const movies = require('./api/movies'); 
+const tvs = require('./api/tvs');     
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/movies/:id?', movies);
+app.get('/tvs/:id?', tvs)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
